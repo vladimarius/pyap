6805cd573d4205860f93fd73861195f706d595e0
Thomas <thomas@thomas.com>
2022-09-06 18:24:02 +0200

    Fix edge cases

--- pyap/__init__.py
+++ pyap/__init__.py
@@ -5,5 +5,6 @@ API hooks
 """
 from .api import parse
 from .utils import match, findall
+from .address import Address
 
-__all__ = ["parse", "match", "findall"]
+__all__ = ["parse", "match", "findall", "Address"]
